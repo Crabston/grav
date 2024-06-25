39e6df52518af3bd275d4f2961172679768944af
Nicholas Krebs <nicholas@nicholas-krebs.ch>
2024-06-25 15:29:49 +0200

    simplify script

--- scripts/trigger-cron.sh
+++ scripts/trigger-cron.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # This script is used to trigger the Grav scheduler via cron, when cron cannot run the PHP CLI directly.
-# Usage: /path/to/trigger-cron.sh /path/to/grav/root
+# Usage: /path/to/grav/scripts/trigger-cron.sh
 
-# Change to the Grav root directory of the site
-path=$1
-cd $path
+# Change to the Grav root directory
+cd "$(dirname "$0")/.."
 
 # Run the Grav scheduler
-/bin/php bin/grav scheduler # 1>> /dev/null 2>&1
+#/bin/php bin/grav scheduler 1>> /dev/null 2>&1
+/bin/php bin/grav scheduler
 
 # Output the last run time for cron
 echo "last run @ $(date +'%d-%Y-%d %H:%M:%S')"
